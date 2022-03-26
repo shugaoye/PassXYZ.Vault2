46cc6a2120c121e84aa155384480d31bdd673307
Roger Ye <shugaoye@yahoo.com>
2022-03-26 14:00:30 +0800

    Subscribing to the Window lifecycle events

--- PassXYZ.Vault/App.xaml.cs
+++ PassXYZ.Vault/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace PassXYZ.Vault;
+﻿using System.Diagnostics;
+
+namespace PassXYZ.Vault;
 
 public partial class App : Application
 {
@@ -8,4 +10,41 @@ public partial class App : Application
 
 		MainPage = new MainPage();
 	}
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        Window window = base.CreateWindow(activationState);
+
+        window.Created += (s, e) =>
+        {
+            Debug.WriteLine("PassXYZ.Vault.App: 1. Created event");
+        };
+
+        window.Activated += (s, e) =>
+        {
+            Debug.WriteLine("PassXYZ.Vault.App: 2. Activated event");
+        };
+
+        window.Deactivated += (s, e) =>
+        {
+            Debug.WriteLine("PassXYZ.Vault.App: 3. Deactivated event");
+        };
+
+        window.Stopped += (s, e) =>
+        {
+            Debug.WriteLine("PassXYZ.Vault.App: 4. Stopped event");
+        };
+
+        window.Resumed += (s, e) =>
+        {
+            Debug.WriteLine("PassXYZ.Vault.App: 5. Resumed event");
+        };
+
+        window.Destroying += (s, e) =>
+        {
+            Debug.WriteLine("PassXYZ.Vault.App: 6. Destroying event");
+        };
+
+        return window;
+    }
 }
