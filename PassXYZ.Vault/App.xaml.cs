18d07d613d1d5c69a24c83ba953c0a9af7cbe507
Roger Ye <shugaoye@yahoo.com>
2022-03-26 14:50:40 +0800

    Consuming the lifecycle override methods

--- PassXYZ.Vault/App.xaml.cs
+++ PassXYZ.Vault/App.xaml.cs
@@ -1,50 +1,14 @@
-﻿using System.Diagnostics;
-
-namespace PassXYZ.Vault;
+﻿namespace PassXYZ.Vault;
 
 public partial class App : Application
 {
 	public App()
 	{
 		InitializeComponent();
-
-		MainPage = new MainPage();
 	}
 
     protected override Window CreateWindow(IActivationState activationState)
     {
-        Window window = base.CreateWindow(activationState);
-
-        window.Created += (s, e) =>
-        {
-            Debug.WriteLine("PassXYZ.Vault.App: 1. Created event");
-        };
-
-        window.Activated += (s, e) =>
-        {
-            Debug.WriteLine("PassXYZ.Vault.App: 2. Activated event");
-        };
-
-        window.Deactivated += (s, e) =>
-        {
-            Debug.WriteLine("PassXYZ.Vault.App: 3. Deactivated event");
-        };
-
-        window.Stopped += (s, e) =>
-        {
-            Debug.WriteLine("PassXYZ.Vault.App: 4. Stopped event");
-        };
-
-        window.Resumed += (s, e) =>
-        {
-            Debug.WriteLine("PassXYZ.Vault.App: 5. Resumed event");
-        };
-
-        window.Destroying += (s, e) =>
-        {
-            Debug.WriteLine("PassXYZ.Vault.App: 6. Destroying event");
-        };
-
-        return window;
+        return new PxWindow(new MainPage());
     }
 }
